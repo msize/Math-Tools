910d3951da9e576b4d28aed489383cfb4484a1fc
Максим Сизоненко <msize@ac-sw.com>
2016-01-25 08:44:43 +0600

    Add test for `Parser`

--- src/test/Parser_test.cpp
+++ src/test/Parser_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
 #include "../Parser.h"
+#include "../SyntaxTreeVisitior.h"
 
 class ParserTest: public ::testing::Test {
 
@@ -13,8 +14,38 @@ protected:
 
 };
 
-TEST_F(ParserTest, parseExpression) {
-  static const std::string val = "2 + 2 * 2";
-  Parser parser(val);
-  EXPECT_STREQ(val.c_str(), parser.getVal().c_str());
+TEST_F(ParserTest, parseExpression1) {
+  static const std::string expression = " 2 * 2 + 2";
+  static const std::string result = "+*CCC";
+  auto operation = Parser::makeOperation(expression);
+  SyntaxTreeVisitior visitor;
+  operation->accept(visitor);
+  EXPECT_STREQ(result.c_str(), visitor.result().c_str());
+}
+
+TEST_F(ParserTest, parseExpression2) {
+  static const std::string expression = "2 + 2 * 2 ";
+  static const std::string result = "+C*CC";
+  auto operation = Parser::makeOperation(expression);
+  SyntaxTreeVisitior visitor;
+  operation->accept(visitor);
+  EXPECT_STREQ(result.c_str(), visitor.result().c_str());
+}
+
+TEST_F(ParserTest, parseExpression3) {
+  static const std::string expression = "20 - (4 + 2 * 3) / 5";
+  static const std::string result = "-C/()+C*CCC";
+  auto operation = Parser::makeOperation(expression);
+  SyntaxTreeVisitior visitor;
+  operation->accept(visitor);
+  EXPECT_STREQ(result.c_str(), visitor.result().c_str());
+}
+
+TEST_F(ParserTest, parseExpression4) {
+  static const std::string expression = "(20 - ((4 + 2) * 3)) / 5";
+  static const std::string result = "/()-C()*()+CCCC";
+  auto operation = Parser::makeOperation(expression);
+  SyntaxTreeVisitior visitor;
+  operation->accept(visitor);
+  EXPECT_STREQ(result.c_str(), visitor.result().c_str());
 }
